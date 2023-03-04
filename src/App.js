371c0804999aeb06bfa901cd4b8a2564086d40b2
Angelo Ilardi <ange448@gmail.com>
2023-03-04 18:33:00 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import Header from './components/Header';
 import Home from './components/pages/Home';
+import ProjectsTab from './components/pages/ProjectsTab';
+import Contacts from './components/pages/Contacts';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="projectsTab" element={<ProjectsTab />} />
+          <Route path='contact' element={<Contacts /> } />
         </Routes>
       </div>
     </Router>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,7 +4,7 @@ function Home(params) {
     return (
         <div>
             <img src="" alt=""></img>
-            <h1>HEY, I'M ANGELO ILARDI!</h1>
+            <h1 className="">HEY, I'M ANGELO ILARDI!</h1>
             <h2>A front-end web developer striving to build stunning Web Apps that people love to use.</h2>
             <h3>here you can find out more about my projects and get in touch with me.</h3>
         </div>
