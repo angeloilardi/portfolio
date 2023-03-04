9c17c1346776f5754e20ca8007353f10a5257897
Angelo Ilardi <ange448@gmail.com>
2023-03-04 16:51:06 +0000

    added navbar and home

---
+++ src/components/Header.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { NavLink } from 'react-router-dom';
+
+function Header() {
+    return (
+        <div>
+            <ul className="nav nav-tabs">
+                <li className="nav-item">
+                    <NavLink
+                        to="/"
+                        end
+                        className={({ isActive }) =>
+                            isActive ? 'nav-link active' : 'nav-link'
+                        }
+                    >
+                        Home
+                    </NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink
+                        to="projects"
+                        className={({ isActive }) =>
+                            isActive ? 'nav-link active' : 'nav-link'
+                        }
+                    >
+                        Projects
+                    </NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink
+                        to="contact"
+                        end
+                        className={({ isActive }) =>
+                            isActive ? 'nav-link active' : 'nav-link'
+                        }
+                    >
+                        Contact
+                    </NavLink>
+                </li>
+            </ul>
+        </div>
+        
+    );
+};
+
+export default Header;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home(params) {
+    return (
+        <div>
+            <img src="" alt=""></img>
+            <h1>HEY, I'M ANGELO ILARDI!</h1>
+            <h2>A front-end web developer striving to build stunning Web Apps that people love to use.</h2>
+            <h3>here you can find out more about my projects and get in touch with me.</h3>
+        </div>
+    )
+    
+}
+
+export default Home;
