1f2e93f836a260b041939ac660014cbadc565fcf
Angelo Ilardi <ange448@gmail.com>
2023-03-06 21:22:36 +0000

    fixed styling

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,9 +7,9 @@ function Home(params) {
             <div class="row align-items-center text-white">
                 <div class="col-lg-4 mt-3"><img className="bio-pic" src={require('./../../images/2420317.jpeg')} alt=""></img></div>
                 <div class="col-lg-8">
-                    <h1 className="">HEY, I'M ANGELO ILARDI!</h1>
-                    <h2>A front-end web developer striving to build stunning Web Apps that people love to use.</h2>
-                    <h3>here you can find out more about my projects and get in touch with me.</h3>
+                    <h1 className="mb-2">HEY, I'M ANGELO ILARDI!</h1>
+                    <h2 className="mb-2">A front-end web developer striving to build stunning Web Apps that people love to use.</h2>
+                    <h3>Here you can find out more about my projects and get in touch with me.</h3>
                 </div>
             </div>
         </div>
