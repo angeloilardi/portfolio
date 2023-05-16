5a0a813fbedd52aee6e0948a015e5a590408a7a4
Angelo Ilardi <ange448@gmail.com>
2023-05-16 21:45:09 +0100

    added projects

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,7 +6,7 @@ function Home(params) {
         <div className="container-fluid bg">
             <div class="row align-items-center text-white">
                 <div class="col-lg-4 mt-3"><img className="bio-pic" src={require('./../../images/2420317.jpeg')} alt=""></img></div>
-                <div class="col-lg-8">
+                <div class="col-lg-8 home-intro">
                     <h1 className="mb-2">HEY, I'M ANGELO ILARDI!</h1>
                     <h2 className="mb-2">A front-end web developer striving to build stunning Web Apps that people love to use.</h2>
                     <h3>Here you can find out more about my projects and get in touch with me.</h3>
--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -5,10 +5,10 @@ import React from "react";
 function Project(props) {
     const { name, description, screenshot, gitHubLink, livePageLink } = props;
     return (
-        <div className="card project p-0 m-3" style={{ width: "22rem" }}>
-            <img src={screenshot} className="card-img-top img-fluid" alt="..." style={{ height: "60%", width: "auto" }} />
+        <div className="card project g-0 m-3 col-lg-3 col-md-6" >
+            <img src={screenshot} className="card-img-top img-fluid" alt="..." style={{ height: "70%", width: "100" }} />
 
-            <div className="card-body">
+            <div className="card-body project-body">
                 <h5 className="card-title">{name}  <a href={livePageLink} target="_blank" rel="noreferrer"><i className="fa-solid fa-arrow-up-right-from-square"></i></a>    <a href={gitHubLink} target="_blank" rel="noreferrer"><i className="fa-brands fa-github"></i></a>
                 </h5>
                 <p className="card-text">{description}</p>
